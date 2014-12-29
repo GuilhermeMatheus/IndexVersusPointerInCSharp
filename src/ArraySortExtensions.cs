79e2a37e674a43159ec055f5dd68657ffe056829
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2014-12-29 11:24:48 -0200

    Initial Commit

---
+++ src/ArraySortExtensions.cs
@@ -0,0 +1,74 @@
+﻿namespace IndexVersusPointerInCSharp
+{
+    public static class ArraySortExtensions
+    {
+        /// <summary>
+        /// Sorts the elements of the sequence in ascending order.
+        /// </summary>
+        /// <param name="arr">The sequence to in-place sort</param>
+        public static void SafeBubbleSort(this int[] arr)
+        {
+            var flag = true;
+            int temp;
+            int numLength = arr.Length;
+
+            for (int i = 1; (i <= (numLength - 1)) && flag; i++)
+            {
+                flag = false;
+                for (int j = 0; j < (numLength - 1); j++)
+                {
+                    if (arr[j + 1] < arr[j])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        flag = true;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sorts the elements of the sequence in ascending order.
+        /// </summary>
+        /// <param name="arr">The sequence to in-place sort</param>
+        public static unsafe void UnsafeBubbleSort(this int[] arr)
+        {
+            var flag = true;
+            int temp;
+            int numLength = arr.Length;
+
+            fixed (int* p = arr)
+            {
+                for (int* pI = p; (pI <= p + (numLength - 1)) && flag; pI++)
+                {
+                    flag = false;
+                    for (int* pJ = p; pJ < p + (numLength - 1); pJ++)
+                    {
+                        if (*(pJ + 1) < *pJ)
+                        {
+                            temp = *pJ;
+                            *pJ = *(pJ + 1);
+                            *(pJ + 1) = temp;
+                            flag = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Checks if a sequence is in ascending order
+        /// </summary>
+        /// <param name="arr">The sequence to check</param>
+        /// <returns>Returns True when the sequence is in ascending order, otherwise, returns False</returns>
+        public static bool CheckAscendingSort(this int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+                if (arr[i] > arr[i + 1])
+                    return false;
+
+            return true;
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using sd = System.Diagnostics;
+
+namespace IndexVersusPointerInCSharp
+{
+    class Program
+    {
+        const int COUNT = 20000;
+
+        static void Main(string[] args)
+        {
+            //We create the worst case scenario for bubble sort.
+            //Thus, the sort will need to make the maximum number of passes and, hence, the maximum number of array access.
+            var arr = Enumerable.Range(0, COUNT)
+                                .OrderByDescending(i => i);
+
+            var safeSource = arr.ToArray();
+            var unsafeSource = arr.ToArray();
+            
+            Stopwatch(safeSource.SafeBubbleSort, "Safe BubbleSort");
+            Stopwatch(unsafeSource.UnsafeBubbleSort, "Unsafe BubbleSort");
+
+            //Asserting the correct results by contract condition.
+            Contract.Assert(safeSource.CheckAscendingSort());
+            Contract.Assert(unsafeSource.CheckAscendingSort());
+
+            Console.Read();
+        }
+
+        static void Stopwatch(Action action, string message)
+        {
+            var sw = sd.Stopwatch.StartNew();
+            action();
+            sw.Stop();
+
+            Console.WriteLine("{0} executed in {1} milliseconds.", message, sw.ElapsedMilliseconds);
+        }
+    }
+}
